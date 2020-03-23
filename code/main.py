ffafe3e2872873f8103609bfb9b8c68073069300
Kontiko <kontiko.fb@gmail.com>
2020-03-23 21:32:09 +0100

    Finished mod adding removing and listing

--- code/main.py
+++ code/main.py
@@ -2,9 +2,9 @@ import sys
 import os
 import shutil
 import json
-def add(data):
-    modname=data[0]
-    filepaths=data[1:]
+config = {}
+def update_config():
+    global config
     try:
         if os.path.dirname(__file__)=="":
             with open('config.json') as json_data_file:
@@ -12,11 +12,16 @@ def add(data):
         else:
             with open(os.path.dirname(__file__)+'/config.json') as json_data_file:
                 config = json.load(json_data_file)
-        
+        return False
         
     except:
         print("can't open Config file")
+        return True
+def add(data):
+    if update_config():
         return
+    modname=data[0]
+    filepaths=data[1:]
     if os.path.exists(config["ksppath"]+"kem/"+modname+"/"):
         print("Mod with these name already exists")
         return
@@ -24,7 +29,18 @@ def add(data):
     for filepath in filepaths:
         print(filepath)
         shutil.copyfile(filepath,config["ksppath"]+"kem/"+modname+"/"+filepath.split("/")[-1])
-
+def remove(modname):
+    if update_config():
+        return
+    if os.path.exists(config["ksppath"]+"kem/"+modname+"/"):
+        shutil.rmtree(config["ksppath"]+"kem/"+modname+"/")
+    else:
+        print(Mod is not Available)
+def list():
+    if update_config():
+        return
+    for mod in [x[0] for x in os.walk(config["ksppath"]+"kem/")][1:]:
+        print(mod.split("/")[-1])
 def help():
     print("help")
 
@@ -32,5 +48,12 @@ if sys.argv[1]=="help":
     help()
 elif sys.argv[1]=="add":
     add(sys.argv[2:])
+elif sys.argv[1]=="remove":
+    if len(sys.argv)==3:
+        remove(sys.argv[2])
+    else:
+        print ("Wrong argument number. Remove has <Name of Mod> as argument")
+elif sys.argv[1]=="list":
+    list()
 else:
     print("Unknown command. try <kme help>")
